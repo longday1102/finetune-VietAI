d58962e3a71372617f6596fbfc9180d86addfebf
Hoàng Long <longhoangai1102@gmail.com>
2023-09-22 18:11:29 +0700

    Update train.py

--- train.py
+++ train.py
@@ -146,6 +146,6 @@ class Trainer:
                 
             
             print("Saving..........")
-            torch.save(model.state_dict(), self.model_name + ".checkpoint")
+            torch.save(model.state_dict(), "{}.checkpoint".format(self.model_name.split("/")[1]))
             print("****************** Save successfully ******************")
         
